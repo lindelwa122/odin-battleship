3e785f0e112ceaa286617f90b89dce147c26fd0f
Lindelwa <lindelwa.codes@gmail.com>
2023-12-16 09:44:23 +0200

    Add gameboard

--- index.js
+++ index.js
@@ -9,4 +9,134 @@ const Ship = (len = 1) => {
   return { getHits, getLength, hit, isSunk }
 }
 
-export { Ship };
+const Gameboard = () => {
+  const _board = [];
+  const _occupiedSpots = [];
+  
+  // Initialize board
+  for (let i = 0; i < 10; i++) {
+    const row = [];
+    for (let j = 0; j < 10; j++) {
+      row[j] = { shipInfo: null, hit: false };
+    }
+    _board.push(row);
+  }
+
+  const receiveAttack = (row, col) => {
+    const spot = _board[row][col];
+    if (!spot.hit && spot.shipInfo) {
+      spot.shipInfo.ship.hit();
+      spot.hit = true;
+    } else if (!spot.hit) {
+      spot.hit = true;
+    }
+  }
+
+  const arrayContains = (mainArr, searchArr) => {
+    for (const item of mainArr) {
+      if (item.length !== searchArr.length) continue;
+  
+      if (item.toString() === searchArr.toString()) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const _isLegal = (spots) => {
+    for (const spot of spots) {
+      const [row, col] = spot;
+
+      if (arrayContains(_occupiedSpots, spot)) {
+        return false;
+      } else if (arrayContains(_occupiedSpots, [row - 1, col])) {
+        return false;
+      } else if (arrayContains(_occupiedSpots, [row + 1, col])) {
+        return false;
+      } else if (arrayContains(_occupiedSpots, [row, col - 1])) {
+        return false;
+      } else if (arrayContains(_occupiedSpots, [row, col + 1])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  const _randCoords = () => ([
+    Math.floor(Math.random() * 10),
+    Math.floor(Math.random() * 10)
+  ]);
+
+  // Random Direction
+  const _randDir = () => {
+    const ran = Math.floor(Math.random() * 2);
+    return ran === 0 ? 'horizontal' : 'vertical';
+  }
+
+  const _randomSpots = (len) => {
+    if (typeof len !== 'number' || len < 0 || len > 4) {
+      throw new Error('The length must be a number and inclusively between 1 and 4');
+    }
+
+    while (true) {
+      const spots = [];
+      const [row, col] = _randCoords();
+      const dir = _randDir();
+      if (dir === 'horizontal' && col + len <= 10) {
+        for (let i = 0; i < len; i++) {
+          const curCol = col + i;
+          spots.push([row, curCol]);
+        }
+      } else if (dir === 'vertical' && row + len <= 10) {
+        for (let i = 0; i < len; i++) {
+          const curRow = row + i;
+          spots.push([curRow, col]);
+        }
+      } else {
+        continue;
+      }
+
+      if (_isLegal(spots)) {
+        spots.forEach(spot => _occupiedSpots.push(spot));
+        return { spots, dir };
+      }
+    }
+  }
+
+  const _ships = [];
+
+  const _createShip = (len) => {
+    if (typeof len !== 'number' || len < 0 || len > 4) {
+      throw new Error('The length must be a number and inclusively between 1 and 4');
+    }
+
+    const ship = Ship(len);
+    _ships.push(ship);
+    const { dir, spots } = _randomSpots(len);
+    spots.forEach((spot, index) => {
+      const [row, col] = spot;
+      _board[row][col].shipInfo = {
+        ship,
+        direction: dir,
+        isStart: index === 0,
+        isEnd: index === len - 1
+      };
+    });
+  }
+
+  // Create _ships
+  for (let i = 1; i <= 4; i++) {
+    for (let j = 0; j < (5 - i); j++) {
+      _createShip(i);
+    }
+  }
+
+  const getBoard = () => _board;
+
+  const isAllSunk = () => _ships.every((ship) => ship.isSunk())
+
+  return { getBoard, isAllSunk, receiveAttack };
+}
+
+export { Ship, Gameboard };
