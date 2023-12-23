286c7c0ba49f6333f8f92c2665dd72ff02370dae
Lindelwa <lindelwa.codes@gmail.com>
2023-12-23 13:27:47 +0200

    Fix bug.
    _validateCoords should return true if the coordinatess are valid as
    the attack function relies on it returning true for it to proceed with
    the attack.

--- index.js
+++ index.js
@@ -235,6 +235,8 @@ const Player = (gameboard, computer = false) => {
     if (_illegalSpots.has(`${row},${col}`)) {
       return false;
     }
+
+    return true;
   }
 
   const _getCoords = () => {
@@ -260,53 +262,53 @@ const Player = (gameboard, computer = false) => {
   return { attack };
 }
 
-const GUI = (() => {
-  const paintBoard = (board, computer = false) => {
-    const grid = document.querySelector(computer ? '.comp-grid' : '.my-grid');
+// const GUI = (() => {
+//   const paintBoard = (board, computer = false) => {
+//     const grid = document.querySelector(computer ? '.comp-grid' : '.my-grid');
 
-    for (let row = 0; row < board.length; row++) {
-      for (let col = 0; col < board[row].length; col++) {
-        const box = grid.querySelector(`.row[data-coord='${row}'] > .col[data-coord='${col}']`);
+//     for (let row = 0; row < board.length; row++) {
+//       for (let col = 0; col < board[row].length; col++) {
+//         const box = grid.querySelector(`.row[data-coord='${row}'] > .col[data-coord='${col}']`);
 
-        const spot = board[row][col];
-        const shipInfo = spot.shipInfo;
-        if (shipInfo && shipInfo.ship.isSunk()) {
-          box.classList.add('sunk');
-        } else if (shipInfo && spot.hit) {
-          box.classList.add('ship', 'hit');
-        } else if (shipInfo && !computer) {
-          box.classList.add('ship');
-        } else if (spot.hit) {
-          box.classList.add('hit');
-        }
-      }
-    }
-  }
+//         const spot = board[row][col];
+//         const shipInfo = spot.shipInfo;
+//         if (shipInfo && shipInfo.ship.isSunk()) {
+//           box.classList.add('sunk');
+//         } else if (shipInfo && spot.hit) {
+//           box.classList.add('ship', 'hit');
+//         } else if (shipInfo && !computer) {
+//           box.classList.add('ship');
+//         } else if (spot.hit) {
+//           box.classList.add('hit');
+//         }
+//       }
+//     }
+//   }
 
-  const getPlayerPosition = () => {
-    const spots = document.querySelectorAll('.comp-grid .col');
+//   const getPlayerPosition = () => {
+//     const spots = document.querySelectorAll('.comp-grid .col');
 
-    return new Promise((resolve) => {
-      const handleClick = (e) => {
-        spots.forEach((spot) => {
-          spot.removeEventListener('click', handleClick);
-        });
+//     return new Promise((resolve) => {
+//       const handleClick = (e) => {
+//         spots.forEach((spot) => {
+//           spot.removeEventListener('click', handleClick);
+//         });
 
-        const ele = e.target;
-        const parent = ele.parentElement;
-        const row = +parent.dataset.coord;
-        const col = +ele.dataset.coord;
-        resolve({row, col});
-      }
+//         const ele = e.target;
+//         const parent = ele.parentElement;
+//         const row = +parent.dataset.coord;
+//         const col = +ele.dataset.coord;
+//         resolve({row, col});
+//       }
 
-      spots.forEach((spot) => {
-        spot.addEventListener('click', handleClick);
-      });
-    })
-  }
+//       spots.forEach((spot) => {
+//         spot.addEventListener('click', handleClick);
+//       });
+//     })
+//   }
 
-  return { getPlayerPosition, paintBoard };
-})();
+//   return { getPlayerPosition, paintBoard };
+// })();
 
 // const Game = (() => {
 //   const _pB = Gameboard(); // playerBoard
