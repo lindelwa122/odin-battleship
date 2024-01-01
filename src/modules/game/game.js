c8826d2607f051c6efe30c3e364b109ffedc7b20
Lindelwa <lindelwa.codes@gmail.com>
2024-01-01 18:00:44 +0200

    Add functionalities to update ship

--- src/modules/game/game.js
+++ src/modules/game/game.js
@@ -72,7 +72,102 @@ const Game = () => {
     _displayWinner();
   }
 
-  return { play };
+  const _findShipStart = (row, col, shipInfo) => {
+    for (let i = 0; i < 4; i++) {
+      let step;
+      if (shipInfo.direction === "horizontal") {
+        step = _board[row][col - i].shipInfo;
+
+        if (step.isStart) {
+          return col - i;
+        }
+      } else {
+        step = _board[row - i][col].shipInfo;
+
+        if (step.isStart) {
+          return row - i;
+        }
+      }
+    }
+  };
+
+  const _getShipInfo = (row, col) => {
+    const shipCoords = [];
+    const board = _pB.getBoard();
+    const shipInfo = board[row][col].shipInfo;
+    const shipLen = shipInfo.ship.getLength();
+    const { rowStart, colStart } = _findShipStart(row, col, shipInfo);
+
+    for (let i = 0; i < shipLen; i++) {
+      if (shipInfo.direction === 'horizontal') {
+        shipCoords.push({ row, col: colStart + i });
+      } else {
+        shipCoords.push({ row: rowStart + i, col });
+      }
+    }
+
+    return { dir: shipInfo.direction, shipCoords };
+  }
+
+  const _validateNewCoords = (row, col, shipLen, dir) => {
+    const isOccupied = (row, col) => {
+      const board = _pB.getBoard();
+      return board[col][row].shipInfo ?? false;
+    }
+
+    const isLegal = (spots) => {
+      for (const spot of spots) {
+        const [row, col] = spot;
+  
+        if (isOccupied(spot)) return false;
+        else if (isOccupied([row - 1, col])) return false;
+        else if (isOccupied([row + 1, col])) return false;
+        else if (isOccupied([row, col - 1])) return false;
+        else if (isOccupied([row, col + 1])) return false;
+        else if (isOccupied([row - 1, col - 1]))  return false;
+        else if (isOccupied([row - 1, col + 1])) return false;
+        else if (isOccupied([row + 1, col - 1])) return false;
+        else if (isOccupied([row + 1, col + 1])) return false;
+      }
+  
+      return true;
+    };
+
+    const  newCoords = []
+    for (let i = 0; i < shipLen; i++) {
+      if (dir === 'horizontal') {
+        newCoords.push({ row, col: col + i });
+      } else {
+        newCoords.push({ row: row + i, col });
+      }
+    }
+
+    return isLegal(newCoords);
+  }
+
+  const updateShipCoords = (row, col, newRow, newCol) => {
+    const board = _pB.getBoard();
+    if (!board[row][col].shipInfo) return false;
+
+    const { shipCoords, dir } = _getShipInfo(row, col);
+
+    if (!_validateNewCoords(newRow, newCol, shipCoords.length, dir)) {
+      return false;
+    }
+
+    for (let i = 0; i < shipCoords.length; i++) {
+      const { row, col } = shipCoords[i];
+      if (dir === 'horizontal') {
+        board[newRow][newCol + i] = board[row][col];
+      } else {
+        board[newRow + i][newCol] = board[row][col];
+      }
+    }
+
+    return true;
+  }
+
+  return { play, updateShipCoords };
 };
 
 export default Game();
