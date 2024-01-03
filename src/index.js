5cd43f5c298b37be0086dae333a7a10221163245
Lindelwa <lindelwa.codes@gmail.com>
2024-01-03 06:40:52 +0200

    Add interface for moving ships.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,20 @@ import "normalize.css";
 import "./style.css";
 import Game from "./modules/game/game";
 
-Game.play();
+const updateCoordsBtn = document.querySelector('.update-coords-btn');
+updateCoordsBtn.addEventListener('click', () => {
+  const oldRow = document.querySelector('.from > .row').value;
+  const oldCol = document.querySelector('.from > .col').value;
+  const newRow = document.querySelector('.to > .row').value;
+  const newCol = document.querySelector('.to > .col').value;
+
+  const updated = Game.updateShipCoords(+oldRow, +oldCol, +newRow, +newCol);
+  if (!updated) {
+    alert("Board wasn't updated, check if your coordinates are correct.");
+  }
+});
+
+const playBtn = document.querySelector('.play-btn');
+playBtn.addEventListener('click', () => {
+  Game.play();
+});
